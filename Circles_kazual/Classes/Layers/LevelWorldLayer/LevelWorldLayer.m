624e4d373038a192f0a6c1dd753d2f73ea18484e
Gogunsky Vladimir <gogunsky.vladimir@gmail.com>
2013-02-07 09:50:46 +0200

    Added animated sun

--- Circles_kazual/Classes/Layers/LevelWorldLayer/LevelWorldLayer.m
+++ Circles_kazual/Classes/Layers/LevelWorldLayer/LevelWorldLayer.m
@@ -13,6 +13,9 @@
 - (void) addSubstrate;
 - (void) addLayers ;
 - (void) addObjects ;
+
+- (void) addActions:(NSArray *)actions forObject:(CCSprite *)object;
+
 @end
 
 @implementation LevelWorldLayer
@@ -37,10 +40,13 @@
 
 - (void) addSubstrate {
     
-    CCLayerColor * layer = [[CCLayerColor alloc] initWithColor:ccc4(255,255,255,255)];
+    CCLayerColor * layer = [[CCLayerColor alloc] initWithColor:ccc4(0,0,0,255)];
     [layer setAnchorPoint:ccp(0,0)];
     [layer setPosition:ccp(0,0)];
-    [self addChild:layer z:-1];
+    [self addChild:layer z:10];
+    [layer setOpacity:0];
+    
+    [layer runAction:[CCRepeatForever actionWithAction:[CCSequence actions:[CCFadeTo actionWithDuration:60 opacity:220],[CCDelayTime actionWithDuration:10], [CCFadeTo actionWithDuration:40 opacity:0],[CCDelayTime actionWithDuration:20], nil]]];
 }
 
 - (void) addLayers {
@@ -51,8 +57,8 @@
         CCSprite * layer = [CCSprite spriteWithFile:[layerInfo objectForKey:WORLD_LAYER_IMAGE]];
         [layer setPosition:ccp(0,0)];
         [layer setAnchorPoint:ccp(0,0)];
-        [self setOpacity:125];
         [self addChild:layer z:[[layerInfo objectForKey:WORLD_LAYER_ORDER] intValue]];
+
     }
 }
 
@@ -65,7 +71,33 @@
         [object setPosition:CGPointFromString([objectInfo objectForKey:WORLD_OBJECT_POSITION])];
         [object setAnchorPoint:ccp(0.5,0.5)];
         [self addChild:object z:[[objectInfo objectForKey:WORLD_OBJECT_ORDER] intValue]];
+        
+        [self addActions:[objectInfo objectForKey:WORLD_OBJECT_ACTIONS] forObject:object];
     }
+}
+
+
+- (void) addActions:(NSArray *)actions forObject:(CCSprite *)object {
+    
+    for (NSArray * sequence in actions) {
+        NSMutableArray * actionsInSequence = [[NSMutableArray alloc] init];
+        for (NSDictionary * action in sequence) {
+      
+            if ([[action objectForKey:WORLD_OBJECT_ACTION_TYPE] isEqualToString:WORLD_OBJECT_ACTION_MOVE_BY]) {
+                CCMoveBy * theAction = [CCMoveBy actionWithDuration:[[action objectForKey:WORLD_OBJECT_ACTION_DURATION] intValue] position:CGPointFromString([action objectForKey:WORLD_OBJECT_ACTION_VALUE])];
+                [actionsInSequence addObject:theAction];
+            } else if ([[action objectForKey:WORLD_OBJECT_ACTION_TYPE] isEqualToString:WORLD_OBJECT_ACTION_DELAY]) {
+                CCDelayTime * theAction = [CCDelayTime actionWithDuration:[[action objectForKey:WORLD_OBJECT_ACTION_DURATION] intValue]];
+                [actionsInSequence addObject:theAction];
+            } else if ([[action objectForKey:WORLD_OBJECT_ACTION_TYPE] isEqualToString:WORLD_OBJECT_ACTION_FADE_TO]) {
+                CCFadeTo * theAction = [CCFadeTo actionWithDuration:[[action objectForKey:WORLD_OBJECT_ACTION_DURATION] intValue] opacity:[[action objectForKey:WORLD_OBJECT_ACTION_VALUE] intValue]];
+                [actionsInSequence addObject:theAction];
+            }
+        }
+        [object runAction:[CCRepeatForever actionWithAction:[CCSequence actionWithArray:actionsInSequence]]];
+        [actionsInSequence release];
+    }
+    
 }
 
 @end
--- Circles_kazual/Classes/Logic/Defines/Defines.h
+++ Circles_kazual/Classes/Logic/Defines/Defines.h
@@ -24,11 +24,18 @@ static NSString * const LOSE_RESULT     = @"LOSE";
 
 #pragma mark - WORLD LAYER KEYS -
 
-static NSString * const WORLD_LAYERS            = @"Layers";
-static NSString * const WORLD_LAYER_IMAGE       = @"imageName";
-static NSString * const WORLD_LAYER_ORDER       = @"order";
+static NSString * const WORLD_LAYERS                    = @"Layers";
+static NSString * const WORLD_LAYER_IMAGE               = @"imageName";
+static NSString * const WORLD_LAYER_ORDER               = @"order";
 
-static NSString * const WORLD_OBJECTS           = @"Objects";
-static NSString * const WORLD_OBJECT_IMAGE      = @"imageName";
-static NSString * const WORLD_OBJECT_ORDER      = @"order";
-static NSString * const WORLD_OBJECT_POSITION   = @"position";
+static NSString * const WORLD_OBJECTS                   = @"Objects";
+static NSString * const WORLD_OBJECT_IMAGE              = @"imageName";
+static NSString * const WORLD_OBJECT_ORDER              = @"order";
+static NSString * const WORLD_OBJECT_POSITION           = @"position";
+static NSString * const WORLD_OBJECT_ACTIONS            = @"actions";
+static NSString * const WORLD_OBJECT_ACTION_TYPE        = @"type";
+static NSString * const WORLD_OBJECT_ACTION_DURATION    = @"duration";
+static NSString * const WORLD_OBJECT_ACTION_VALUE       = @"value";
+static NSString * const WORLD_OBJECT_ACTION_MOVE_BY     = @"moveBy";
+static NSString * const WORLD_OBJECT_ACTION_DELAY       = @"delay";
+static NSString * const WORLD_OBJECT_ACTION_FADE_TO     = @"fadeTo";
